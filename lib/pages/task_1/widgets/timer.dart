291d4177d5b2cd7d75deaeb489c29340e6537986
Musoxon <msx.tuit@gmail.com>
2024-05-16 01:46:02 +0500

    Timer App is finished.

--- lib/pages/task_1/widgets/timer.dart
+++ lib/pages/task_1/widgets/timer.dart
@@ -1,7 +1,19 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class TimerTask extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_day_16/utils/extensions.dart';
+
+class TimerTask extends StatefulWidget {
   const TimerTask({super.key});
+
+  @override
+  State<TimerTask> createState() => _TimerTaskState();
+}
+
+class _TimerTaskState extends State<TimerTask> {
+  int initialSeconds = 30;
+  int remainingSeconds = 0;
+  bool isStarted = false;
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +22,73 @@ class TimerTask extends StatelessWidget {
         title: const Text('Timer App'),
         leading: const SizedBox(),
         centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+                child: Text(
+              '$remainingSeconds',
+              style: const TextStyle(fontSize: 50),
+            )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      if (initialSeconds > 0) initialSeconds--;
+                    });
+                  },
+                  icon: const Icon(Icons.remove),
+                ),
+                Text(
+                  '$initialSeconds second${initialSeconds > 1 ? 's' : ''}',
+                  style: const TextStyle(fontSize: 30),
+                ),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      initialSeconds++;
+                    });
+                  },
+                  icon: const Icon(Icons.add),
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            onPressed: !isStarted
+                ? () {
+                    setState(() {
+                      isStarted = true;
+                      remainingSeconds = initialSeconds;
+                    });
+
+                    Timer.periodic(
+                      const Duration(seconds: 1),
+                      (timer) {
+                        setState(
+                          () {
+                            if (remainingSeconds > 0) {
+                              remainingSeconds--;
+                            } else {
+                              timer.cancel();
+                              isStarted = false;
+                            }
+                          },
+                        );
+                      },
+                    );
+                  }
+                : null,
+            child: const Text('START'),
+          ),
+          20.height,
+        ],
       ),
     );
   }
